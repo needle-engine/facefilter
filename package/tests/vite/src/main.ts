dad394d37330b01c93b6a715a44fda63bda9ef87
Marcel Wiessler <marcel@gaisterhand.de>
2025-04-14 17:53:48 +0200

    minor

--- package/tests/vite/src/main.ts
+++ package/tests/vite/src/main.ts
@@ -3,21 +3,28 @@ import "@needle-tools/facefilter";
 import { NeedleTrackingManager } from "@needle-tools/facefilter";
 
 
-onStart(() => {
+onStart((ctx) => {
+    ctx.menu.showQRCodeButton(true);
     const t = NeedleTrackingManager.instance;
     console.log(t);
     if (t) {
-        setTimeout(() => {
-            t.maxHands = 2;
-            setTimeout(() => {
-                t.maxFaces = 0;
-                setTimeout(() => {
-                    t.maxHands = 1;
-                    setTimeout(()=> {
-                        t.maxHands = 0;
-                    }, 2000)
-                }, 2000)
-            }, 3000)
-        }, 3000)
+        t.maxFaces = 0;
+        t.maxHands = 20;
+        // setTimeout(() => {
+        //     t.maxHands = 2;
+        //     setTimeout(() => {
+        //         t.maxFaces = 0;
+        //         setTimeout(() => {
+        //             t.maxHands = 1;
+        //             setTimeout(()=> {
+        //                 t.maxHands = 0;
+        //                 setTimeout(()=> {
+        //                     t.maxFaces = 1;
+        //                     t.maxHands = 2;
+        //                 }, 2000)
+        //             }, 2000)
+        //         }, 2000)
+        //     }, 3000)
+        // }, 3000)
     }
 })
